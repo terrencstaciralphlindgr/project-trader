e0ec94ee462bcafe3e09cab494395640a25e3983
DKS <daniel.saisani@kcl.ac.uk>
2022-10-06 11:08:00 +0100

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,21 +1,22 @@
 """Sends an email"""
 import smtplib
-info = open("info.txt","r+")
-data=info.readlines()
-try:
-    username, password = data[0],data[1]
-except:
-    print("Login details are not entered correctly!")
-    quit()
 def notify(body="Untitled Body",contact="danielsaisani@gmail.com",subject="Untitled Subject"):
+        try:
+            server = smtplib.SMTP("smtp.gmail.com:587")
+            server.ehlo()
+            server.starttls()
+            server.login(username,password)
+        except:
+            print("[!] Connection to email sever was not established!")
+            return False
+        message = "Subject:"+subject+"\n\n"+body
+        server.sendmail(username,contact,message)
+        return True
+if __main__=="__main__":
+    info = open("info.txt","r+")
+    data=info.readlines()
     try:
-        server = smtplib.SMTP("smtp.gmail.com:587")
-        server.ehlo()
-        server.starttls()
-        server.login(username,password)
+        username, password = data[0],data[1]
     except:
-        print("[!] Connection to email sever was not established!")
-        return False
-    message = "Subject:"+subject+"\n\n"+body
-    server.sendmail(username,contact,message)
-    return True
+        print("Login details are not entered correctly!")
+        quit()
